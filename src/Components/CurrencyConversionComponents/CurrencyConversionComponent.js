ceba27b64e9c9ce007668347f37af818451decab
Gary Clark <gclark04432@live.co.uk>
2020-03-30 12:41:33 +0100

    can render first rate on component screen

--- src/Components/CurrencyConversionComponents/CurrencyConversionComponent.js
+++ src/Components/CurrencyConversionComponents/CurrencyConversionComponent.js
@@ -1,20 +1,33 @@
 import React, { useState, useEffect } from 'react';
 
 function CurrencyConversion() {
-    const [baseCurrency, setBaseCurrency] = useState("GBP");
-    const [requestedCurrency, setRequestedCurrency] = useState("EUR");
+  const [baseCurrency, setBaseCurrency] = useState("GBP");
+  const [requestedCurrency, setRequestedCurrency] = useState("EUR");
+  const [conversionRates, setConversionRates] = useState({});
 
-    useEffect(() => {
-      fetch(`https://api.exchangeratesapi.io/latest?base=${baseCurrency}`)
-        .then(res => res.json())
-        .then(data => console.log(data))
-    })
+  useEffect(() => {
+    fetch(`https://api.exchangeratesapi.io/latest?base=${baseCurrency}`)
+      .then(res => res.json())
+      .then(data => setConversionRates(data.rates))
+    },[])
+
+    function displayRates(){
+      if (conversionRates.length === 0) return null;
+      for (let latestRate in conversionRates){
+        console.log(conversionRates);
+        return (
+            <li>{latestRate}</li> )
+      }
+    }
 
     return (
       <section className="currency-conversion">
         <h3>{baseCurrency} to {requestedCurrency}</h3>
+        <ul>
+        { displayRates() }
+        </ul>
       </section>
     )
-}
+  }
 
-export default CurrencyConversion;
+  export default CurrencyConversion;
